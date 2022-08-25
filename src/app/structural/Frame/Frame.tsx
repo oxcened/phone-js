4d224acea7d6cd8e77965e1b4bc1ad5c1c24f939
Alen Ajam <alen.ajam@live.com>
2022-08-26 01:28:15 +0200

    refactor frame sizing

--- src/app/structural/Frame/Frame.tsx
+++ src/app/structural/Frame/Frame.tsx
@@ -1,9 +1,40 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useCallback, useEffect, useRef, useState } from 'react';
 import './Frame.css';
 
 const Frame = ({ children }: PropsWithChildren) => {
+  const frame = useRef<HTMLDivElement>(null);
+  const [rect, setRect] = useState<DOMRect>();
+
+  useEffect(() => {
+    setRect(frame.current?.getBoundingClientRect());
+  }, [frame]);
+
+  const updateZoom = useCallback(() => {
+    if (!rect) {
+      return;
+    }
+
+    const scale = Math.min(
+      window.innerWidth / (rect.width ?? 0),
+      window.innerHeight / (rect.height ?? 0)
+    );
+
+    (document.getElementsByClassName('frame')[0] as any).style.transform = `scale(${scale})`;
+  }, [rect])
+
+  useEffect(() => {
+    updateZoom();
+    const listener = () => {
+      updateZoom()
+    };
+
+    window.addEventListener('resize', listener);
+
+    return () => window.removeEventListener('resize', listener);
+  }, [updateZoom]);
+
   return (
-    <div className='frame'>
+    <div className='frame' ref={frame}>
       <div className='notch'>
         <div className='camera'></div>
       </div>
