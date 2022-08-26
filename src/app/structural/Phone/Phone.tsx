cc9baf89daad8eb01cbb5923304a77b6a3780f29
Alen Ajam <alen.ajam@live.com>
2022-08-26 09:44:33 +0200

    removed outlet import

--- src/app/structural/Phone/Phone.tsx
+++ src/app/structural/Phone/Phone.tsx
@@ -1,7 +1,6 @@
 import React, { PropsWithChildren } from 'react';
 import Frame from '../Frame/Frame';
 import StatusBar from '../StatusBar/StatusBar';
-import { Outlet } from 'react-router-dom';
 
 const Phone = (props: PropsWithChildren) => {
   return (
