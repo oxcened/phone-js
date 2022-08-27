0927827f942c7cd160e5acb404a08e65d75d5739
Alen Ajam <alen.ajam@live.com>
2022-08-27 02:36:33 +0200

    cleaned css rules, added props documentation

--- src/app/structural/PhoneAppScreen/PhoneAppScreen.tsx
+++ src/app/structural/PhoneAppScreen/PhoneAppScreen.tsx
@@ -4,9 +4,16 @@ import StatusBar from '../StatusBar/StatusBar';
 import HomeButton from '../HomeButton/HomeButton';
 
 type Props = {
+  /** Toggles dark mode - Determinates color of status bar items and home button */
   isDarkMode?: boolean;
+
+  /** Toggles safe area - Places margins to avoid notch and screen corners in general */
   useSafeArea?: boolean;
+
+  /** Toggles status bar in the top screen */
   showStatusBar?: boolean;
+
+  /** Toggles home button in the bottom screen */
   showHomeButton?: boolean;
 };
 
